67c9870815e3113a86b587038fe868043417b095
ridha-arlian <ridhaarlian@proton.me>
2026-08-27 10:26:35 +0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+  app: {
+    head: {
+      title: 'Ridha Arlian',
+      // meta: [
+      //   { name: 'description', content: 'Cari titik temu waktu meeting untuk tim remote lintas zona waktu, tanpa login, tanpa database.' },
+      // ],
+    },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    // '@nuxt/eslint',
+    '@nuxt/fonts',
+    // '@nuxtjs/color-mode',
+  ],
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui'
+  },
+  fonts: {
+    families: [
+      { name: 'IBM Plex Sans', provider: 'google' },
+      { name: 'IBM Plex Mono', provider: 'google' },
+      { name: 'Fraunces', provider: 'google'},
+    ]
+  },
+})
